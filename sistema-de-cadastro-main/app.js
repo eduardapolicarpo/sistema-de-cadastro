0d92ea31612c65d64fc200fd835a13b368cadd24
eduardapolicarpo <142759892+eduardapolicarpo@users.noreply.github.com>
2023-11-08 01:01:39 -0300

    Add files via upload

---
+++ sistema-de-cadastro-main/app.js
@@ -0,0 +1,5 @@
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O Paulo fez esse comit sem você ver na aula!")
+cosole.log("Enviando para o Github")
